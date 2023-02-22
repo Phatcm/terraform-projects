c77190dd388d9a4a80a4004f53dc27126352b4cc
Phat Cao <phat.caominh.dev@gmail.com>
2023-02-22 21:01:46 +0700

    Created sns file

---
+++ sns.tf
@@ -0,0 +1,13 @@
+#create an sns topic
+#terraform aws create sns topic
+resource "aws_sns_topic" "user_updates"{
+    name = "dev-sns-topic"
+}
+
+#create an sns topic subcription
+#terraform aws sns topic subcription
+resource "aws_sns_topic_subcription" "notification_topic" {
+    topic_arn = aws_sns_topic.user_updates.arn
+    protocol = "email"
+    endpoint = var.operator_email
+}
